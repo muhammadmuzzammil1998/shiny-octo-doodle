429144e3356478a2ac4034c43a0337c607435f8c
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2017-08-26 09:06:29 +0530

    Update

---
+++ startsWith($something, $string).php
@@ -0,0 +1,7 @@
+function startsWith($haystack, $needle) {
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+}
+
+function endsWith($haystack, $needle) {
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
+}
