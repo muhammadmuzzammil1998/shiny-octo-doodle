7437077eafdbe786b35913bb457d18a7ba1b0c2a
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2017-09-01 10:33:35 +0530

    Added rgb2hex function
    arg => rgb array:
    rgb[0] => r
    rgb[1] => g
    rgb[2] => b

---
+++ php/rgb2hex.php
@@ -0,0 +1,7 @@
+function rgb2hex($rgb) {
+    $hex = "#";
+    $hex .= str_pad(dechex($rgb[0]), 2, "0", STR_PAD_LEFT);
+    $hex .= str_pad(dechex($rgb[1]), 2, "0", STR_PAD_LEFT);
+    $hex .= str_pad(dechex($rgb[2]), 2, "0", STR_PAD_LEFT);
+    return $hex; 
+}
