338c47af8d71dbfc81c617e21e4862b2be78e346
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-01-11 14:58:14 +0530

    Update startsWith($something, $string).php

--- php/startsWith($something, $string).php
+++ php/startsWith($something, $string).php
@@ -1,7 +1,9 @@
-function startsWith($haystack, $needle) {
-    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
-}
+<?php
+    function startsWith($haystack, $needle) {
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+    }
 
-function endsWith($haystack, $needle) {
-    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
-}
+    function endsWith($haystack, $needle) {
+        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
+    }
+ ?>
