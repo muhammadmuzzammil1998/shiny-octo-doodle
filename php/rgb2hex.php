db896ab3915297c19f78a28c8a4a3a379f4507ad
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-01-11 14:58:09 +0530

    Update rgb2hex.php

--- php/rgb2hex.php
+++ php/rgb2hex.php
@@ -1,7 +1,9 @@
-function rgb2hex($rgb) {
-    $hex = "#";
-    $hex .= str_pad(dechex($rgb[0]), 2, "0", STR_PAD_LEFT);
-    $hex .= str_pad(dechex($rgb[1]), 2, "0", STR_PAD_LEFT);
-    $hex .= str_pad(dechex($rgb[2]), 2, "0", STR_PAD_LEFT);
-    return $hex; 
-}
+<?php
+    function rgb2hex($rgb) {
+        $hex = "#";
+        $hex .= str_pad(dechex($rgb[0]), 2, "0", STR_PAD_LEFT);
+        $hex .= str_pad(dechex($rgb[1]), 2, "0", STR_PAD_LEFT);
+        $hex .= str_pad(dechex($rgb[2]), 2, "0", STR_PAD_LEFT);
+        return $hex; 
+    }
+ ?>
